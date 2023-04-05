4537aed7f22649d01109b43ce377c0eb4ebf9579
Владислав Хлыбов <vavan8765@gmail.com>
2023-04-05 14:02:00 +0300

    Добавьте файлы проекта.

---
+++ AKBarsMedApp/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AKBarsMedApp.Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AKBarsMedApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static akbmeddbEntities akbmeddbEntities = new akbmeddbEntities();
+    }
+}
---
+++ AKBarsMedApp/View/AuthorizationWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using AKBarsMedApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AKBarsMedApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AutorizationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user = App.akbmeddbEntities.User.Where(x => x.Login == LoginTB.Text && x.Password == PassPB.Password).FirstOrDefault();
+                if (user != null)
+                {
+                    new HomeWindow().Show();
+                    MessageBox.Show("Вы успешно авторизованы");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+        }
+    }
+}
